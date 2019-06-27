8c024b62a8c1fe97b2d059a87188543fcb9c99f3
lijie1224 <793590413@qq.com>
2019-06-27 22:05:40 +0800

    lij

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        System.out.println( "这是新上传的呀" );
     }
+
 }
--- src/test/java/test/AppTest.java
+++ src/test/java/test/AppTest.java
@@ -1,8 +1,11 @@
 package test;
 
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//import org.junit.Test;
 
 /**
  * Unit test for simple App.
