5fb82404547011051929c9cc99cd28b5693eb6f2
lijie1224 <793590413@qq.com>
2019-06-27 22:41:41 +0800

    lij2

---
+++ src/main/java/test/NewTest1.java
@@ -0,0 +1,9 @@
+package test;
+
+public class NewTest1 {
+    public static void main( String[] args )
+    {
+        System.out.println( "这是新上传的呀" );
+        System.out.println( "再传一遍" );
+    }
+}
